d7eedfb28590f3e02a32331ce2bdf4c89b1fbf5f
pritamSteinn <preetambhalerao00@gmail.com>
2019-12-09 15:15:03 +0530

    test cases changed

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,17 +1,16 @@
-const {User} = require('../../../models/users');
+const { User } = require('../../../models/users');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const momgoose = require('mongoose');
+const mongoose = require('mongoose');
 describe('User.generateAuthToken', () => {
-    it('Should return a valid JWT', ()=> {
-        const payload = {
-            _id: new mongoose.Types.ObjectId().toHexString(),
-            isAdmin: true
-        };
-        const user = new User(payload);
-        const token =  user.generateAutheToken();
-        const decoded = jwt.vefify(token, config.get('jwtPrivateKey'));
-        expect(decoded).toMatchObject(payload);
-    })
-
-})
+  it('Should return a valid JWT', () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
+    };
+    const user = new User(payload);
+    const token = user.generateAutheToken();
+    const decoded = jwt.vefify(token, config.get('jwtPrivateKey'));
+    expect(decoded).toMatchObject(payload);
+  });
+});
