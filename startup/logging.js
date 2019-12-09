f012e7af1ef64becc6ae9bb49879a7fb4212f5bd
pritamSteinn <preetambhalerao00@gmail.com>
2019-12-09 18:12:54 +0530

    resolving errors in winston virsion

--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
-require('winston-mongodb');
+// require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
@@ -13,10 +13,8 @@ module.exports = function() {
   });
 
   winston.add(winston.transports.File, { filename: 'logfile.log' });
-  winston.add(
-    new winston.transports.MongoDB({
-      db: 'mongodb://localhost/vidly',
-      level: 'info'
-    })
-  );
+  // winston.add(winston.transports.MongoDB, {
+  //   db: 'mongodb://localhost/vidly',
+  //   level: 'info'
+  // });
 };
